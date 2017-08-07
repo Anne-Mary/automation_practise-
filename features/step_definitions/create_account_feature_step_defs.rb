1e89f95c059380449aae054abb0e418b31b3703a
Tech-A32 <Tech-A32@tech-a32s-air.home>
2017-08-07 21:31:21 +0100

    added a new file for step definitions on creating an account on the site

---
+++ features/step_definitions/create_account_feature_step_defs.rb
@@ -0,0 +1,23 @@
+Given(/^I access the homepage$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+And(/^click sign in$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I fill in the available field for create account$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+And(/^I click create account$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+And(/^I fill all the required fields$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I have a new account$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
